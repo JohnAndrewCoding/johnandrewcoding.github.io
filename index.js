4a4bc7444bef7d63cfb4a6315cbcb560f2061483
John Andrew Self <81932054+JohnAndrewCoding@users.noreply.github.com>
2024-11-26 12:11:02 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Check if the ticker is fully visible, if not, clone the first item and append it to the end of the list
+
 function checkTicker() {
   const ticker = document.getElementById('news-ticker').querySelector('ul');
   const firstItem = ticker.querySelector('li:first-child');
@@ -16,6 +16,26 @@ function Get(yourUrl){
   Httpreq.send(null);
   return Httpreq.responseText;
 }
+
+function sportsGames(){
+
+let espnUrl = "https://site.api.espn.com/apis/site/v2/sports/football/college-football/scoreboard?dates=20241119-20241123";
+var scoreboard = JSON.parse(Get(espnUrl));
+const newsList = document.getElementByID("news-list");
+for(let i = 0; i < scoreboard['events'].length; i++){
+let homeTeam = scoreboard['events'][i]['competitions'][0]['competitors'][0]['team']['location'];
+let awayTeam = scoreboard['events'][i]['competitions'][0]['competitors'][1]['team']['location'];
+let gameString = `${homeTeam} vs ${awayTeam}`;
+const li = document.createElement("li");
+li.textContent = gameString;
+newsList.appendChild(li);
+
+}
+
+
+
+}
 // Check the ticker on window load and resize
 window.addEventListener('load', checkTicker);
 window.addEventListener('resize', checkTicker);
+
