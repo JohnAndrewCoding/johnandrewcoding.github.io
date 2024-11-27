857bee12ab214c47eb1f06f2002428056d42997a
John Andrew Self <81932054+JohnAndrewCoding@users.noreply.github.com>
2024-11-27 10:27:33 -0500

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,23 @@ newsList.appendChild(li);
 document.getElementById("news-ticker").appendChild(newsList);
 
 }
+let cfbUrl = "https://site.api.espn.com/apis/site/v2/sports/football/college-football/scoreboard?dates=20241130";
+var cfbData = JSON.parse(Get(cfbUrl));
 
+let nflUrl = "https://site.api.espn.com/apis/site/v2/sports/football/nfl/scoreboard?dates=20241126-20241201";
+var nflData = JSON.parse(Get(nflUrl));
+
+let nbaUrl = "https://site.api.espn.com/apis/site/v2/sports/basketball/nba/scoreboard?dates=20241127";
+var nbaData = JSON.parse(Get(nbaUrl));
+
+const favoriteTeams = document.getElementById("favorite-teams");
+const texasImg = cfbData[47]['competitions'][0]['competitors'][1]['team']['logo'];
+const bucsImg =  nflData[9]['competitions'][0]['competitors'][1]['team']['logo'];
+const lakersImg = nbaData[10]['competitions'][0]['competitors'][1]['team']['logo'];
+
+favoriteteams.appendChild(texasImg);
+favoriteteams.appendChild(bucsImg);
+favoriteteams.appendChild(lakersImg);
 
 function checkTicker() {
   const ticker = document.getElementById('news-ticker').querySelector('ul');
