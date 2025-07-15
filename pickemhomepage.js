0c72adac460d788816fc1e7c293217e1f92ee4bc
John Andrew Self <81932054+JohnAndrewCoding@users.noreply.github.com>
2025-07-15 10:02:05 -0400

    Update pickemhomepage.js

--- pickemhomepage.js
+++ pickemhomepage.js
@@ -1,1 +1,23 @@
 document.getElementById("content").innerHTML = "Javascript file";
+
+const apiUrl = 'https://site.api.espn.com/apis/site/v2/sports/football/college-football/scoreboard?dates=20250829';
+
+// Make a GET request
+fetch(apiUrl)
+  .then(response => {
+    // Check if the network response was successful
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    // Parse the JSON data from the response
+    return response.json();
+  })
+  .then(data => {
+    document.getElementById("content").innerHTML = data['events'][47]['competitions'][0]['status']['type']['detail']
+    // Handle the retrieved data
+    console.log(data);
+  })
+  .catch(error => {
+    // Handle any errors that occurred during the fetch operation
+    console.error('Error:', error);
+  });
