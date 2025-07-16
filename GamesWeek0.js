015e818ac84cd2b280daa6a0dc6e92aa9a558615
John Andrew Self <81932054+JohnAndrewCoding@users.noreply.github.com>
2025-07-16 09:26:52 -0400

    Update GamesWeek0.js

--- GamesWeek0.js
+++ GamesWeek0.js
@@ -9,7 +9,7 @@ fetch('https://site.api.espn.com/apis/site/v2/sports/football/college-football/s
   })
   .then(data => {
     // Handle the retrieved data
-    for(let i = 0; i < data.events.length; i ++) {
+    for(let i = 0; i < 3; i ++) {
     const homeTeam = document.createTextNode(data.events[i].competitions[0].competitors[0].team.location);
     const versus = document.createTextNode(" vs. ");
     const awayTeam = document.createTextNode(data.events[i].competitions[0].competitors[1].team.location);
