372678f8d17fb312c81ed865fba4caec2ed79ca5
JohnAndrewCoding <andrew.self31@gmail.com>
2025-07-18 17:37:29 -0400

    Lets go

--- pickemhomepage.js
+++ pickemhomepage.js
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -14,8 +14,8 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const db = firestore();
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
 
 db.collection("userInfo").add({
     user: "Andrew",
