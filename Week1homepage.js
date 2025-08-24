03cdf379ddde833ac17a50d96f381f8d7b968c98
John Andrew Self <81932054+JohnAndrewCoding@users.noreply.github.com>
2025-08-24 11:21:01 -0400

    Update Week1homepage.js

--- Week1homepage.js
+++ Week1homepage.js
@@ -1,5 +1,17 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAkSlyFKQNHYQgLa_dQuzjYSzXSISoCWKU",
+  authDomain: "college-football-pickem-68eed.firebaseapp.com",
+  projectId: "college-football-pickem-68eed",
+  storageBucket: "college-football-pickem-68eed.appspot.com",
+  messagingSenderId: "650202039805",
+  appId: "1:650202039805:web:70e51177aab22e4d614594"
+};
 
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
 
 // Load games + picks
 async function loadGamesAndPicks(weekNum) {
