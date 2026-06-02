34fd7d9c58aade51686d5382763cfd2fc580d0e9
John Andrew Self <81932054+JohnAndrewCoding@users.noreply.github.com>
2026-06-02 16:02:26 -0400

    Update sw.js

--- 2026_College_Pickem/sw.js
+++ 2026_College_Pickem/sw.js
@@ -1,11 +1,1 @@
-// No more manual service worker registrations or conversion math here!
-<!-- PushAlert Unified Code -->
-<script type="text/javascript">
-    (function(d, t) {
-        var g = d.createElement(t),
-        s = d.getElementsByTagName(t)[0];
-        g.src = "https://cdn.pushalert.co/unified_1f01dca52f87f02d3c59cdddb88f5c30.js";
-        s.parentNode.insertBefore(g, s);
-    }(document, "script"));
-</script>
-<!-- End PushAlert Unified Code -->
+importScripts("https://cdn.pushalert.co/sw-89694_2.js");
